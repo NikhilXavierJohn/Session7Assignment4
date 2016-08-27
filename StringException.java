e1b66ecc82194d7b6f9e36fd963570835173373f
NikhilXavierJohn <nkh.john@gmail.com>
2016-08-27 16:00:23 +0530

    Create StringException.java

---
+++ StringException.java
@@ -0,0 +1,27 @@
+public class StringException {
+
+	public static void main(String[] args) {
+		
+		String a = "This is an example";
+		String b = null;
+		
+		try{
+			
+			for(int i = 0; i <= a.length(); i++){
+				
+				b = b + a.charAt(i);
+				
+			}
+			
+			
+		}
+		catch(Exception e){
+			
+			System.out.println("Exception Handled :"+e);
+			
+		}
+		
+		
+	}
+	
+}
